44684d8c454e3599470b6bf16b6d8efdffea1e7d
nadaelarabyy <nadaelarabyy@yahoo.com>
2019-03-01 01:55:23 +0200

    l

--- routes/api/events.js
+++ routes/api/events.js
@@ -16,11 +16,18 @@ router.get('/',async(req,res)=>{
 // get a single event
 router.get('/:id',async(req,res)=>{
     const id = req.params.id
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
+        // it's an ObjectID    
+    
     const event = await Event.findById(id)
     if(event){
       return res.json({data: event})
+    }else{
+        return res.json({msg:"it was rendering here"});
     }
-    res.status(400).json({msg:`an event with id ${id} not found`});
+}
+ else{
+    return res.status(400).json({msg:`an event with id ${id} not found`});}
 });
 // create an Event
 
@@ -43,14 +50,22 @@ router.post('/',async(req,res)=>{
 
 router.put('/:id',async(req,res)=>{
     try {
-        
+        if ((req.params.id).match(/^[0-9a-fA-F]{24}$/)){
+        const found=await Event.findById(req.params.id);
         const isValidated = validator.updateValidationEvent(req.body)
         if (isValidated.error) {
             console.log(2);
             res.status(400).send({ error: isValidated.error.details[0].message })
         }
         await Event.findByIdAndUpdate({_id: req.params.id}, req.body);
-        res.json({msg: 'Event Request updated successfully'});
+        if(found){
+          res.json({msg: 'Event Request updated successfully'});
+        }else{
+            return res.status(404).send({error: 'Event does not exist'})
+        }
+    }else{
+        return res.status(404).send({error: 'Event does not exist'})
+    }
        }
        catch(error) {
            console.log(error)
@@ -62,8 +77,19 @@ router.put('/:id',async(req,res)=>{
 router.delete('/:id',async(req,res)=>{
     try {
         const id = req.params.id
+        if ((id).match(/^[0-9a-fA-F]{24}$/)){
+        const found=await Event.findById(req.params.id);
+        if(found){
         const deletedEvent = await Event.findByIdAndRemove(id)
         res.json({msg:'Event was deleted successfully', data: deletedEvent})
+        }else{
+            return res.json({msg:`event with id ${id} not found`});
+        }
+
+        }
+        else{
+            return res.json({msg:`event with id ${id} not found`});
+        }
        }
        catch(error) {
            console.log(error)
