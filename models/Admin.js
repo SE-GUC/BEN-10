c1eeddf953a582164ea80ba8606819512e50e2e2
Ahmedsamhar <ahmed.samhar@student.guc.edu.eg>
2019-02-28 20:33:08 +0200

    Samhar v2

--- models/Admin.js
+++ models/Admin.js
@@ -7,12 +7,52 @@ const AdminSchema = new Schema({
         type: String,
         required: true
     },
+    gender: {
+        type: boolean,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    maritalStatus: {
+        type: String,
+        required: true
+    },
+    militaryStatus: {
+        type: String,
+        required: true
+    },
+    drivingLicense: {
+        type: number,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    area: {
+        type: String,
+        required: true
+    },
+    postalcode: {
+        type: number,
+        required: true
+    },
+    mobileNumber: {
+        type: number,
+        required: true
+    },
     email: {
         type: String,
         required: true
     },
     birthdate: {
-        type: date,
+        type: Date,
         required: true
     },
     password: {
