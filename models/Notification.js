fe7eb6997b39cff9b191b3d3d8d1e6c3a5595d50
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-02 11:07:54 +0200

    edit

--- models/Notification.js
+++ models/Notification.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const eventme  = require('EventRequest.js')
 // Create the schema
 const NotificationSchema = new Schema({
     description: {
