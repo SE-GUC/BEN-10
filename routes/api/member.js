5e098b954432f8dd92b45c898a455f27008018a4
AhmedAbdel-Aal <a7med_201196@yahoo.com>
2019-03-02 14:17:51 -0800

    CRUD of member

--- routes/api/member.js
+++ routes/api/member.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 const member = require('../../models/member')
-const validator = require('../../validations/notificationsValidation')
-const notificationValidator = require('../../validations/memberValidations')
+const notificationValidator = require('../../validations/notificationsValidation')
+const validator = require('../../validations/memberValidations')
 
 
 // GET method to retrieve all members
@@ -43,9 +43,6 @@ router.put('/:id', async (req,res) => {
      if(!mem) return res.status(404).send({error: 'member is not found'})
      const isValidated = validator.updateValidation(req.body)
      if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
- 
-     const updatedmember = await member.updateOne(req.body)
-
 
      res.json({msg: 'member updated successfully'})
     }
@@ -61,11 +58,6 @@ router.delete('/:id', async (req,res) => {
     try {
      const id = req.params.id
      const deletedmember = await member.findByIdAndRemove(id)
-     res.json({msg:'Book was deleted successfully', data: deletedmember})
-
-
-
-
      if(!deletedmember) return res.status(404).send({error: 'member is not found'})
      res.json({msg:'member was deleted successfully', data: deletedmember})
     }
