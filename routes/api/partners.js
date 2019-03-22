c1a635e6fbde7b7372ee4f39e7b2914d1360b9ec
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-22 19:14:33 +0200

    no

--- routes/api/partners.js
+++ routes/api/partners.js
@@ -82,16 +82,17 @@ router.delete('/:id', async (req,res) => {
     }  
  })
 
- 
+ router.get('/:id/showFinalDraft', async (req,res)=> {
+    const id = req.params.id
     if(ObjectId.isValid(id))
     {
         const j = await getProjects(id);
-        res.status(200).send(j) ;
+        res.json({data: j}) ;
     }
     else {
         return res.status(404).send({ error: "ID NOT FOUND" })
     }
-    
+
     
   });
 
@@ -99,16 +100,19 @@ router.delete('/:id', async (req,res) => {
 
 
  async function getProjects(partnerid) {
+     var result;
     await fetch(`${server}/api/projects`)
       .then(res => res.json())
       .then(json => {
         const projects = json.data;
-        const hisProjects = projects.filter(m => m.companyID === partnerid && m.life_cycle=="Final review");
+        const hisProjects = projects.filter(m => m.companyID === partnerid && m.life_cycle==="Final review");
+        result = hisProjects;
         return hisProjects;
       })
       .catch(err => console.log("Error", err));
-    return hisProjects;
+    return result;
   }
+  getProjects('5c93d983f3fe6358b41ccd7a');
 
 
 module.exports = router
