e0b8b0a20fab8145830be278c50381d68785e478
omarkhaled998 <omarkhaled98@hotmail.com>
2019-03-22 21:58:14 +0200

    Update member.js

--- routes/api/member.js
+++ routes/api/member.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const member = require('../../models/member')
-
+const fetch = require("node-fetch");
+const server = require("../../config/config");
 const validator = require('../../validations/memberValidations')
+const ObjectId = require('mongodb').ObjectID;
 const notificationValidator = require('../../validations/notificationsValidation')
 
 
@@ -88,5 +90,62 @@ router.delete('/:id', async (req,res) => {
         console.log(error)
     }
  })
+
+
+ //As a candidate I want to book a place in an event (based on the event’s type).
+ router.put('/:id1/bookEvent/:id2',async (req,res) => {
+            const canId=req.params.id1;
+            const  eventId =req.params.id2;
+    if(ObjectId.isValid(canId) && ObjectId.isValid(eventId)){
+        const mem = await member.findById(canId);
+        const event = await Event.findById(eventId);
+        if (mem && event){
+            const type=event.eventType;
+            var skills= mem.skill_set;
+            var allowed =skills.includes(type);
+            if(allowed===true){
+                const members=event.bookedMembers;
+                members.push(canId);
+                const j=await bookEvent(eventId,members);
+                res.status(200).send(j);
+            }
+            else{
+                res.send({msg: "You can't book this event"})
+            }
+        }
+        else
+            return res.status(404).send({error: 'invalid ID'})
+    }else{
+        return res.status(404).send({error: 'invalid ID'})
+    }
+
+})
+
+
+async function bookEvent(eid,members){
+
+    var error = true;
+    const body = { bookedMembers: members };
+    var j;
+    await fetch(`${server}/api/events/${eid}`, {
+            method: 'put',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => {
+        if(res.status === 200){
+            error = false;
+        }
+        return res.json()
+    })
+    .then(json => {
+        if(!error){
+            json = { msg: 'Event booked successfully'}
+        }
+        j = json;
+    })
+    .catch((err) => console.log("Error",err));
+    return j;
+}
 
  module.exports = router
