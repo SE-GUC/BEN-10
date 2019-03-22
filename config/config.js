563b6b80d708d29a5f5dde17798e5b5eea7a1982
mariambakhaty <mariam.bakhaty@hotmail.com>
2019-03-22 22:10:34 +0200

    mariam

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-const server = dev ? 'http://localhost:9000' : 'https://lirtenhub.herokuapp.com/';
+const server = dev ? 'http://localhost:10000' : 'https://lirtenhub.herokuapp.com/';
 module.exports = server
