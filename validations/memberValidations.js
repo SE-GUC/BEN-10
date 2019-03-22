4f5074ccfe58066b8e39fe52f43ab8fcb6b77622
omarkhaled998 <omarkhaled98@hotmail.com>
2019-03-22 22:14:10 +0200

    Update memberValidations.js

--- validations/memberValidations.js
+++ validations/memberValidations.js
@@ -26,7 +26,8 @@ module.exports = {
             Mobile_number:Joi.string().required(),
             Alternative_Mobile_number:Joi.string(),
             events: Joi.array().items(Joi.objectId()),
-            projects: Joi.array().items(Joi.objectId())
+            projects: Joi.array().items(Joi.objectId()),
+            skill_set:Joi.array().items(Joi.string())
         }
 
         return Joi.validate(request, createSchema)
@@ -57,7 +58,8 @@ module.exports = {
             Mobile_number:Joi.string(),
             Alternative_Mobile_number:Joi.string(),
             events: Joi.array().items(Joi.objectId()),
-            projects: Joi.array().items(Joi.objectId())
+            projects: Joi.array().items(Joi.objectId()),
+            skill_set:Joi.array().items(Joi.string())
 
         }
 
