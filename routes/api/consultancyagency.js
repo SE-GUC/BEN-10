13cf0ea285d2926cdf85ff34da3c1cf2cab90f8b
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-22 14:26:17 +0200

    not complete

--- routes/api/consultancyagency.js
+++ routes/api/consultancyagency.js
@@ -1,120 +1,145 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const ObjectId = require('mongodb').ObjectID;
-mongoose.set('useFindAndModify', false);
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const ObjectId = require("mongodb").ObjectID;
+mongoose.set("useFindAndModify", false);
 //FETCH REQUIERMENTS
-const fetch = require('node-fetch');
-const server = require('../../config/config');
+const fetch = require("node-fetch");
+const server = require("../../config/config");
 
-const ConsultancyAgency = require('../../models/ConsultancyAgency')
-const validator = require('../../validations/consultancyagencyValidations')
+const ConsultancyAgency = require("../../models/ConsultancyAgency");
+const validator = require("../../validations/consultancyagencyValidations");
 
-router.get('/', async (req,res) => {
-    const consultancyAgency = await ConsultancyAgency.find()
-    res.json({data: consultancyAgency})
+router.get("/", async (req, res) => {
+  const consultancyAgency = await ConsultancyAgency.find();
+  res.json({ data: consultancyAgency });
+});
+
+router.get("/:id", async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    const id = req.params.id;
+    const consultancyagencys = await ConsultancyAgency.findById(id);
+    if (!consultancyagencys)
+      return res.status(404).send({ error: "Consultancy Agency not exist" });
+    res.json({ data: consultancyagencys });
+  } else {
+    return res.status(404).send({ error: "Consultancy Agency does not exist" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const isValidated = validator.createValidationconsultancyagency(req.body);
+    if (isValidated.error)
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
+    const newConsultancyAgency = await ConsultancyAgency.create(req.body);
+    res.json({
+      msg: "Consultancy Agency was created successfully",
+      data: newConsultancyAgency
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Error");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const isValidated = validator.updateValidationconsultancyagency(req.body);
+      if (isValidated.error)
+        return res
+          .status(400)
+          .send({ error: isValidated.error.details[0].message });
+      const updatedconsultancyagency = await ConsultancyAgency.findByIdAndUpdate(
+        { _id: req.params.id },
+        req.body
+      );
+      if (!updatedconsultancyagency)
+        return res
+          .status(404)
+          .send({ error: "Consultancy Agency does not exist" });
+      res.json({ msg: "Consultancy Agency updated successfully" });
+    } else {
+      return res
+        .status(404)
+        .send({ error: "Consultancy Agency does not exist" });
+    }
+  } catch {
+    console.log(error);
+    return res.status(404).send({ error: "Consultancy Agency does not exist" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      const deletedConsultancyAgency = await ConsultancyAgency.findByIdAndRemove(
+        id
+      );
+      if (!deletedConsultancyAgency)
+        return res
+          .status(400)
+          .send({ error: "Consultancy Agency does not exist" });
+      res.json({
+        msg: "Consultancy Agency was deleted successfully",
+        data: deletedConsultancyAgency
+      });
+    } else {
+      return res
+        .status(404)
+        .send({ error: "Consultancy Agency does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Error");
+  }
+});
+
+//2.2 --As a consultancy agency I want to assign one of the candidates who applied for the task/project.
+
+router.get('/:id/applyingMembers/:pid', async (req,res)=>{
+
+
 })
+getApplyingMembers()
+async function getApplyingMembers(){
+    fetch(`${server}/api/applications`)
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
 
-router.get('/:id', async (req,res) => {
-    if(ObjectId.isValid(req.params.id)){
-    const id = req.params.id
-    const consultancyagencys = await ConsultancyAgency.findById(id)
-    if (!consultancyagencys) return res.status(404).send({error: 'Consultancy Agency not exist'})
-    res.json({data: consultancyagencys})
-    }
-    else{
-        return res.status(404).send({error: 'Consultancy Agency does not exist'})
-    }
+async function assignCandidate(projectID, candidatID) {
+  const body = { memberID: candidatID };
+  var error = true;
 
-})
-
-router.post('/', async (req,res) => {
-    try {
-     const isValidated = validator.createValidationconsultancyagency(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const newConsultancyAgency = await ConsultancyAgency.create(req.body)
-     res.json({msg:'Consultancy Agency was created successfully', data: newConsultancyAgency})
-    }
-    catch(error) {
-        console.log(error)
-        return res.status(400).send('Error')
-    }
-      
- })
-
-
- router.put('/:id', async (req,res) => {
-    try{
-        if(ObjectId.isValid(req.params.id)){
-        const isValidated = validator.updateValidationconsultancyagency(req.body)
-        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-        const updatedconsultancyagency = await ConsultancyAgency.findByIdAndUpdate({_id: req.params.id}, req.body)
-        if (!updatedconsultancyagency) return res.status(404).send({error: 'Consultancy Agency does not exist'})
-        res.json({msg: 'Consultancy Agency updated successfully'})
-        }
-        else{
-        return res.status(404).send({error: 'Consultancy Agency does not exist'})
-        }
-        
-    }
-    catch{
-        console.log(error)
-        return res.status(404).send({error: 'Consultancy Agency does not exist'})
-    }
-
-})
-
- 
-router.delete('/:id', async (req,res) => {
-    try {
-        if(ObjectId.isValid(req.params.id)){
-        const id = req.params.id
-        const deletedConsultancyAgency= await ConsultancyAgency.findByIdAndRemove(id)
-        if(!deletedConsultancyAgency) return res.status(400).send({ error: 'Consultancy Agency does not exist'})
-        res.json({msg:'Consultancy Agency was deleted successfully', data: deletedConsultancyAgency})
-        }else{
-            return res.status(404).send({error: 'Consultancy Agency does not exist'})
-
-        }
-    }
-    catch(error) {
-        console.log(error)
-        return res.status(400).send('Error')
-
-    }  
- })
-//As a consultancy agency I want to assign one of the candidates who applied for the task/project.
- async function assignCandidate(projectID,candidatID){
-
-    const body = { 'memberID': candidatID };
-    var error = true;
-
-    await fetch (`${server}/api/projects/${projectID}`, {
-        method: 'put',
-        body:    JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-    })
+  await fetch(`${server}/api/projects/${projectID}`, {
+    method: "put",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" }
+  })
     .then(res => {
-        if(res.status === 200){
-            error = false;
-        }
-        console.log(res.status)
-        if(!error){
-            result = res
-        }
-        return res.json()
+      if (res.status === 200) {
+        error = false;
+      }
+      console.log(res.status);
+      if (!error) {
+        result = res;
+      }
+      return res.json();
     })
     .then(json => {
-        if(!error){
-            json = { msg: 'Candidate is assigned successfully'}
-        }
-        console.log(json)
-        
+      if (!error) {
+        json = { msg: "Candidate is assigned successfully" };
+      }
+      console.log(json);
     })
-    .catch((err) => console.log("Error",err));
+    .catch(err => console.log("Error", err));
+}
 
- }
+// assignCandidate("5c7a795e53f1ba0c1b351f75", "5c92fffe676da108728b0def");
 
- assignCandidate('5c7a795e53f1ba0c1b351f75','5c92fffe676da108728b0def');
-
- module.exports = router
+module.exports = router;
