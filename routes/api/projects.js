ba5db082da817f2437be19038d3111cc61031197
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-23 10:09:55 +0200

    make it better

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
 mongoose
 .connect(db,{ useNewUrlParser: true })
 .then(() => console.log('Connected to MongoDB'))
