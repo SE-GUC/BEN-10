d531e0dc109669d2bd3bd55fdc2cd585792b5967
mariambakhaty <mariam.bakhaty@hotmail.com>
2019-03-23 15:18:37 +0200

    mariam

--- routes/api/partners.js
+++ routes/api/partners.js
@@ -154,10 +154,12 @@ router.put('/:id1/AssignCAtoProject/:id2',async (req,res) => {
         const consultancy = await ConsultancyAgency.findById(caId);
         if (project && consultancy){
            var consul= project.applyingConsultancies;
+           console.log(consul)
            var found =consul.includes(caId);
+           console.log(found)
            var need = project.want_consultancy;
-        if(need == true){  
-            console.log(consul)
+           console.log(need)
+        if(need == true){ 
             if(found==true){
              const j=await assigning(caId,consultancy.name,projID);
              res.status(200).send(j);
