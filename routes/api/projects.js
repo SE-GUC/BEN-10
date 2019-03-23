7a182e255e546b31236486970f1fc2658c36fd65
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-23 22:44:02 +0200

    alt+shf+F

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,110 +1,114 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const fetch = require('node-fetch');
-const Project = require('../../models/Project')
-const validator = require('../../validations/projectValidations')
-const ObjectId = require('mongodb').ObjectID;
-mongoose.set('useFindAndModify', false);
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const fetch = require("node-fetch");
+const Project = require("../../models/Project");
+const validator = require("../../validations/projectValidations");
+const ObjectId = require("mongodb").ObjectID;
+mongoose.set("useFindAndModify", false);
 
+router.get("/", async (req, res) => {
+  const projects = await Project.find();
+  res.json({ data: projects });
+  // if(req.query.Member_id!=null){
+  //   member_id=req.query.Member_id;
 
-router.get('/', async (req,res) => {
-    const projects = await Project.find();
-    res.json({data: projects})
-    // if(req.query.Member_id!=null){
-    //   member_id=req.query.Member_id;
-
-    // }
-
-})
+  // }
+});
 
 // get a project
-router.get('/:id', async (req,res) => {
-    if(ObjectId.isValid(req.params.id)){
-    const id = req.params.id
-    const projects = await Project.findById(id)
-    if (!projects) return res.status(404).send({error: 'Project does not exist'})
-    res.json({data: projects})
-    }
-    else{
-        return res.status(404).send({error: 'Project does not exist'})
-    }
-
-})
-
-
+router.get("/:id", async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    const id = req.params.id;
+    const projects = await Project.findById(id);
+    if (!projects)
+      return res.status(404).send({ error: "Project does not exist" });
+    res.json({ data: projects });
+  } else {
+    return res.status(404).send({ error: "Project does not exist" });
+  }
+});
 
 // Create a projectt
-router.post('/', async (req,res) => {
-   try {
+router.post("/", async (req, res) => {
+  try {
     console.log(req.body.description);
-    const isValidated = validator.createValidation(req.body)
+    const isValidated = validator.createValidation(req.body);
     if (isValidated.error) {
-        return res.status(400).send({ error: isValidated.error.details[0].message })
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
     }
-    const newProject = await Project.create(req.body)
-    res.json({msg:'Project was created successfully', data: newProject})
-   }
-   catch(error) {
-        return res.status(400).send({ error: "not a project id" });
-   }  
-})
+    const newProject = await Project.create(req.body);
+    res.json({ msg: "Project was created successfully", data: newProject });
+  } catch (error) {
+    return res.status(400).send({ error: "not a project id" });
+  }
+});
 
 // Update a project
-router.put('/:id', async (req,res) => {
-    try {
-        if(ObjectId.isValid(req.params.id))
-            {
-            const isValidated = validator.updateValidation(req.body)
-            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-            const updatedProject = await Project.findByIdAndUpdate({_id: req.params.id}, req.body)
-            if (!updatedProject) return res.status(404).send({error: 'Project does not exist'})
-            res.json({msg: 'Project updated successfully'})
-        }
-        else {
-            return res.status(404).send({ error: "not a project id" })
-        }
+router.put("/:id", async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const isValidated = validator.updateValidation(req.body);
+      if (isValidated.error)
+        return res
+          .status(400)
+          .send({ error: isValidated.error.details[0].message });
+      const updatedProject = await Project.findByIdAndUpdate(
+        { _id: req.params.id },
+        req.body
+      );
+      if (!updatedProject)
+        return res.status(404).send({ error: "Project does not exist" });
+      res.json({ msg: "Project updated successfully" });
+    } else {
+      return res.status(404).send({ error: "not a project id" });
     }
-    catch{
-        console.log(error)
-        return res.status(404).send({ error: "not a project id" })
+  } catch {
+    console.log(error);
+    return res.status(404).send({ error: "not a project id" });
+  }
+});
+
+// Delete a project
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedProject = await Project.findByIdAndRemove(id);
+    if (!deletedProject)
+      return res.status(400).send({ error: "Project does not exist" });
+    res.json({ msg: "Project was deleted successfully", data: deletedProject });
+
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      const deletedProject = await Project.findByIdAndRemove(id);
+      if (!deletedProject)
+        return res.status(400).send({ error: "Project does not exist" });
+      res.json({
+        msg: "Project was deleted successfully",
+        data: deletedProject
+      });
+    } else {
+      return res.status(404).send({ error: "not a project id" });
     }
- })
 
- // Delete a project
- router.delete('/:id',async(req,res)=>{
-    try {
-        const id = req.params.id
-        const deletedProject = await Project.findByIdAndRemove(id)
-        if(!deletedProject) return res.status(400).send({ error: 'Project does not exist'})
-        res.json({msg:'Project was deleted successfully', data: deletedProject})
-
-        if(ObjectId.isValid(req.params.id)){
-        const id = req.params.id
-        const deletedProject = await Project.findByIdAndRemove(id)
-        if(!deletedProject) return res.status(400).send({ error: 'Project does not exist'})
-        res.json({msg:'Project was deleted successfully', data: deletedProject})}
-        else{
-            return res.status(404).send({ error: "not a project id" })
-        }
-       
-
-        if(ObjectId.isValid(req.params.id)){
-            const id = req.params.id
-            const deletedProject = await Project.findByIdAndRemove(id)
-            if(!deletedProject) return res.status(400).send({ error: 'Project does not exist'})
-            res.json({msg:'Project was deleted successfully', data: deletedProject})
-       }else{
-            return res.status(404).send({error: 'Project does not exist'})
-       }
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      const deletedProject = await Project.findByIdAndRemove(id);
+      if (!deletedProject)
+        return res.status(400).send({ error: "Project does not exist" });
+      res.json({
+        msg: "Project was deleted successfully",
+        data: deletedProject
+      });
+    } else {
+      return res.status(404).send({ error: "Project does not exist" });
     }
-       catch(error) {
-           console.log(error)
-           return res.status(400).send('not a project id')
-   
-       }  
-    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("not a project id");
+  }
+});
 
- 
-
-module.exports = router
+module.exports = router;
