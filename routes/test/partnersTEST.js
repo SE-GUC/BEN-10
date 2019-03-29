effe7584a611b9d22dc94df9752c2eeabb8096f3
omarkhaled998 <omarkhaled98@hotmail.com>
2019-03-30 00:08:50 +0200

    Update partnersTEST.js

--- routes/test/partnersTEST.js
+++ routes/test/partnersTEST.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 const AbstractTests = require('./AbstractTests')
-const Admin = require('../../models/admins') //require your model
+const partner = require('../../models/PartnerInfo') //require your model
 const ObjectId = require('mongoose');
 
 class PaTest extends AbstractTests {
@@ -16,13 +16,14 @@ class PaTest extends AbstractTests {
     try {
       return new Promise((resolve, reject) => {
         describe('Making sure A routes work', () => {
-          this.postRequest()
-          this.getRequest()
-          this.putRequest()
-          this.deleteRequest()
+          // this.postRequest()
+          // this.getRequest()
+          // this.putRequest()
+          // this.deleteRequest()
            //1.5 AS a partner i want to review the final work of the candidate who is working on my task/project.
-           this.getMyProjects()
-          // add all methods
+         //  this.getMyProjects()
+         // this.getMyProjectsFail()
+           // add all methods
 
         })
         resolve()
@@ -59,7 +60,10 @@ class PaTest extends AbstractTests {
 
   getMyProjects(){
     test(`get ${this.base_url}`, async () => {
-      const response = await fetch(`${this.base_url}/5c786899f8a8e026447d212f/ShowFinalDraft`, {
+      const part = await partner.find();
+      const part1 = part[0];
+      const partid = part1.id;
+      const response = await fetch(`${this.base_url}/${partid}/ShowFinalDraft`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
       })
@@ -72,6 +76,23 @@ class PaTest extends AbstractTests {
   
     })
   }
+  
+  getMyProjectsFail(){
+    test(`get ${this.base_url}`, async () => {
+      const response = await fetch(`${this.base_url}/5c786899f8ab7d212f/ShowFinalDraft`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      console.log("response stastus: "+ response.status)
+      const jsonResponse = await response.json()
+      console.log(jsonResponse)
+
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+  
+    })
+  }
+
 
 }
-module.exports = Patest
+module.exports = PaTest
