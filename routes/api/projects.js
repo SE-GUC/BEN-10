1218d78e3f770465c2afb5cacf242d230eb5de9f
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-03-31 09:53:10 +0200

    Commit

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -9,7 +9,7 @@ mongoose.set("useFindAndModify", false);
 
 router.get("/", async (req, res) => {
   const projects = await Project.find();
-  res.json({ data: projects });
+  res.status(200).json({ data: projects });
 });
 
 // get a project
@@ -51,7 +51,7 @@ router.put("/:id", async (req, res) => {
         return res
           .status(400)
           .send({ error: isValidated.error.details[0].message });
-      const updatedProject = await Project.findByIdAndUpdate(
+      const updatedProject = await Project.findOneAndUpdate(
         { _id: req.params.id },
         req.body
       );
--- routes/test/partnersTEST.js
+++ routes/test/partnersTEST.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch')
 const AbstractTests = require('./AbstractTests')
-const Admin = require('../../models/admins') //require your model
+const Partners = require("../../models/PartnerInfo");
+const Events = require('../../models/Event')
+const Projects = require("../../models/Project")
 const ObjectId = require('mongoose');
 
 class PaTest extends AbstractTests {
@@ -16,12 +18,17 @@ class PaTest extends AbstractTests {
     try {
       return new Promise((resolve, reject) => {
         describe('Making sure A routes work', () => {
-          this.postRequest()
-          this.getRequest()
-          this.putRequest()
-          this.deleteRequest()
+          // this.postRequest()
+          // this.getRequest()
+          // this.putRequest()
+          // this.deleteRequest()
           // add all methods
-
+          this.Partnerrequestrating()
+          this.PartnerrequestratingbywrongPartnerID()
+          this.PartnerrequestratingbynotavalidatedPartnerID()
+          this.PartnerrequestratingbywrongEventID()
+          this.PartnerrequestratingbynotavalidatedEventID()
+          this.PartnerrequestratingbynotanEventsOwner()
         })
         resolve()
       })
@@ -29,31 +36,122 @@ class PaTest extends AbstractTests {
   }
 
 
-  postRequest () {
-    const requestBody = {
-       // enter model attributes
-    }
-
-    test(`post ${this.base_url}`, async () => {
-      const response = await fetch(`${this.base_url}`, {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-        headers: { 'Content-Type': 'application/json' }
-      })
-      console.log("response stastus: "+ response.status)
-      const jsonResponse = await response.json()
-
-      expect(Object.keys(jsonResponse)).toEqual(['msg','data'])
-      expect(response.status).toEqual(200)
-
-     
-      
-    })
-  }
-
+  postRequest () {}
   getRequest  () {}
   putRequest  () {}
   deleteRequest  () {}
 
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  Partnerrequestrating(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:id/rating/:eid/`, async () => {
+    const events = await Events.find()
+    const pas = await Partners.find()
+    var ids =[]
+    var i=0
+    for(i;i<pas.length;i++){
+      ids.push(pas[i].id.toString())        
+    }
+    var i=0
+    var result =[]
+    for(i;i<events.length;i++){
+      if(ids.includes(events[i].requestorId.toString())){
+        result.push(events[i])
+      }
+    }
+    const pid = result[0].requestorId
+    const eid = result[0].id
+    const response = await fetch(`${this.base_url}/${pid}/rating/${eid}/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['msg'])
+      expect(response.status).toEqual(200)
+    })
+  }
+
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  PartnerrequestratingbywrongPartnerID(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:pid/rating/:eid/`, async () => {
+    const response = await fetch(`${this.base_url}/5c786899f8a8e826447d212f/rating/5c9cda328acdd615d268e514/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+    })
+  }
+
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  PartnerrequestratingbynotavalidatedPartnerID(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:pid/rating/:eid/`, async () => {
+    const response = await fetch(`${this.base_url}/5c786899f8a8e26447d212f/rating/5c9cda328acdd615d268e514/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+    })
+  }
+
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  PartnerrequestratingbywrongEventID(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:pid/rating/:eid/`, async () => {
+    const response = await fetch(`${this.base_url}/5c786899f8a8e026447d212f/rating/5c9cda328acdd615d868e514/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+    })
+  }
+
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  PartnerrequestratingbynotavalidatedEventID(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:pid/rating/:eid/`, async () => {
+    const response = await fetch(`${this.base_url}/5c786899f8a8e026447d212f/rating/5c9cda328add615d268e514/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+    })
+  }
+
+  // 10 As a patrner I want to give the attendees a form to rate the event and give a feedback
+  PartnerrequestratingbynotanEventsOwner(){
+    const requestBody = {}
+  
+  test(`post ${this.base_url}/:pid/rating/:eid/`, async () => {
+    const response = await fetch(`${this.base_url}/5c7a5ae73c28ff08583304ee/rating/5c9cda328add615d268e514/`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const jsonResponse = await response.json()
+      expect(Object.keys(jsonResponse)).toEqual(['error'])
+      expect(response.status).toEqual(404)
+    })
+  }
+
 }
-module.exports = Patest
+module.exports = PaTest
