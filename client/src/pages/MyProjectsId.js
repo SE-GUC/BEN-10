a832dd5dd7bbf39afac88c72364e6da817e5de9c
omarkhaled998 <omarkhaled98@hotmail.com>
2019-04-07 22:26:31 +0200

    Update MyProjectsId.js

--- client/src/pages/MyProjectsId.js
+++ client/src/pages/MyProjectsId.js
@@ -1,20 +1,24 @@
-import React,{ Component } from "react";
-import DeleteProjectId from "../components/DeleteProjectId"
-export default class MyProjectsId extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      id:this.props.id,
-      partner_id:this.props.partner_id
 
+
+import React,{Component} from "react";
+import Project from "../components/getProjectId/getProjectId.js";
+
+
+class MyProjectsId extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+        projectID:this.props.match.params.id}
     }
-  }
-  render(){
-    console.log(this.state.partner_id)
-    return (
-      <DeleteProjectId  id={this.state.id} partner_id={this.state.partner_id}/>
-      
 
-    );
-  }
+    render(){
+        return(
+       <div className="App">
+            <Project id={this.state.projectID}/>
+       </div>
+
+        );
+    }
 }
+export default MyProjectsId;
