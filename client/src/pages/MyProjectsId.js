032bc255942b69645b6251e327e6f45c6a3fffc7
omarkhaled998 <omarkhaled98@hotmail.com>
2019-04-07 09:35:36 +0200

    Update MyProjectsId.js

---
+++ client/src/pages/MyProjectsId.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import Project from "./components/getProjectId/getProjectId.js";
+
+
+class MyProjectsId extends Component{
+
+    render(){
+        return(
+       <div className="App">
+            <Project/>
+       </div>
+
+        );
+    }
+}
+export default MyProjectsId;
