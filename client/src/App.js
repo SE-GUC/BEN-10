02b9b86f398b3e1affddb9e08e645c393f5ae58b
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-04-08 00:17:27 +0200

    fix 3

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
       return (
         <BrowserRouter>
                      <Route exact path="/MyProjects" render={(props) => <MyProjects {...props} partner_id={this.state.partner_id}/>}/> 
-          <Route exact path="/MyProjects" render={(props) => <MyProjects {...props} partner_id={this.state.partner_id} partner_name={this.state.partner_name} />}/>
+          <Route exact path="/MyProjects/:id" render={(props) => <MyProjectsId {...props} partner_id={this.state.partner_id} partner_name={this.state.partner_name} />}/>
              <Route exact path="/MyProject/edit/:id" component={EditMyProject} /> 
 
             <Route exact path="/myEvents/:id" render={(props) => <MyEventsId {...props} partner_id={this.state.partner_id} />} />
