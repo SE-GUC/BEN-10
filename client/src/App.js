67de1dc364664febccc23e519cf6ec346f296ab8
omarkhaled998 <47185702+omarkhaled998@users.noreply.github.com>
2019-04-07 19:31:36 +0200

    Revert "React/omar2"

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
-// import MyProjects from "./pages/MyProjects";
-import MyEventsId from "./pages/MyEventsId";
-
+import MyProjects from "./pages/MyProjects";
 import axios from "axios";
-import { BrowserRouter, Route } from "react-router-dom";
-import MyProjectsId from "./pages/MyProjectsId";
+import { BrowserRouter as Router } from "react-router-dom";
+import Route  from "react-router-dom/Route";
+
 
 
 class App extends Component {
   state = {
-    partner_id: null
+    partner_id:null,
+    partner_name:null
   };
-
+  
   componentDidMount() {
-    axios
-      .get("http://localhost:5000/api/partners")
-      .then(res => {
-        return res.data;
-      })
-      .then(a =>
-        this.setState({
-          partner_id: a.data[a.data.length - 1]._id,
-          partner_name: a.data[0].name
-        })
-      );
+    axios.get("http://localhost:5000/api/partners")
+    .then(res => {
+      return res.data;
+    })
+    .then(a => this.setState({ partner_id: a.data[0]._id, partner_name:a.data[0].name }));
   }
 
   render() {
-    if (this.state.partner_id !== null) {
-      return (
-        <BrowserRouter>
-            <Route path="/myEvents/:id" component={MyEventsId} />
-            <Route path="/myProject/:id" component={MyProjectsId} />
-        </BrowserRouter>
-      );
-    } else {
-      return <div>Loading....</div>;
+    if(this.state.partner_id!==null){
+    return (
+      <Router>
+        <div className="App">
+              <Route path="/MyProjects" render={(props) => <MyProjects {...props} partner_id={this.state.partner_id} partner_name={this.state.partner_name} />}/>
+        </div>
+      </Router>
+    );
+    }else{
+      return(
+        <Router>
+        <div className="App">
+        <Route path="/MyProjects" render={
+        ()=>{
+          return <label>Loading...</label>
+        }
+      }
+      />
+      </div>
+      </Router>
+      )
     }
   }
 }
+
 export default App;
