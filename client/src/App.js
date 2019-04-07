b60a6baaa381736a5db42c62b0d4152599b57b43
omarkhaled998 <omarkhaled98@hotmail.com>
2019-04-07 15:59:21 +0200

    Update App.js

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+// import MyProjects from "./pages/MyProjects";
+import MyEventsId from "./pages/MyEventsId";
+
+import axios from "axios";
+import { BrowserRouter, Route } from "react-router-dom";
+import MyProjectsId from "./pages/MyProjectsId";
+
+
+class App extends Component {
+  state = {
+    partner_id: null
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/partners")
+      .then(res => {
+        return res.data;
+      })
+      .then(a =>
+        this.setState({
+          partner_id: a.data[a.data.length - 1]._id,
+          partner_name: a.data[0].name
+        })
+      );
+  }
+
+  render() {
+    if (this.state.partner_id !== null) {
+      return (
+        <BrowserRouter>
+            <Route path="/myEvents/:id" component={MyEventsId} />
+            <Route path="/myProject/:id" component={MyProjectsId} />
+        </BrowserRouter>
+      );
+    } else {
+      return <div>Loading....</div>;
+    }
+  }
+}
+export default App;
