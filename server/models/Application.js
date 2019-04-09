431f65d615498a64ff100b71a329b35c71744d8d
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-04-09 21:25:44 +0200

    Update Application.js

--- server/models/Application.js
+++ server/models/Application.js
@@ -7,45 +7,9 @@ const ApplicationSchema = new Schema({
         type: Schema.Types.ObjectId,ref:"member",
         required: false
     },
-    applicantName: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    mobile:{
-        type:Number,
-        required:true
-    },
     applyingDate: {
         type: Date,
         required: true
-    },
-    skills: {
-        type: String,
-        required: true
-    },
-    yearsOfExp:{
-        type: Number,
-        required: true
-    },
-    hasJob:{
-        type: Boolean,
-        required: true
-    },
-    activeTasks:{
-        type: Number,
-        required:false
     },
     projectId: {
         type: Schema.Types.ObjectId,ref:"projects",
