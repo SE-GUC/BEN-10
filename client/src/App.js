c383e80f9ce8edf0a28121ed22df339232d5c51f
omarkhaled998 <omarkhaled98@hotmail.com>
2019-04-12 21:55:07 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,165 +1,110 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { blue } from '@material-ui/core/colors/blue';
-import purple from '@material-ui/core/colors/purple';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import { unstable_Box as Box } from '@material-ui/core/Box';
-import { display } from '@material-ui/system';
+import React, { Component } from "react";
+import {
+  Button,
+  Spinner,
+  Card
+} from "react-bootstrap";
+import axios from "axios";
+//import {withRouter} from "react-router-dom";
 
-const styles = {
-  card: {
-    minWidth: 300,
-    width: 50,
-    background:blue,
-    align:'center',
-    margin:blue,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 25,
-    primary :purple,
-
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  gridList: {
-    width: 300,
-    height: 100,
-  },
-  griditem: {
-    text:'10px',
-  },
-};
-
-class SimpleCard  extends React.Component {
-
-  constructor(props){
+class feedBackSending extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-    eventId : this.props.id ,
-    event : [] , 
-    isLoaded : false,
-    bookedmemID:[],
-    members:[],
-    ID:[],
-    CA:null,
-    partner:null,
-    admin:null,
-    type:[],
-    show:false
+      isLoaded: false,
+      items: null,
+      owner: null,
+      submitClicked: false,
+      showMessage:null,
+      eventId:this.props.id
     };
   }
-  async componentDidMount(){
-    await fetch('http://localhost:5000/api/events/5c93e86e61fb9b030dc88b9e').then(res=>res.json())
-    .then(proj=>this.setState({event:proj.data,bookedmemID:proj.data.bookedMembers,ID:proj.data.requestorId}))
-    
-    if(this.state.bookedmemID.length!==0){
-    for(var i=0;i<this.state.bookedmemID.length;i++){
-      await fetch('http://localhost:5000/api/members/'+this.state.bookedmemID[i]).then(res=>res.json())
-    .then(proj=>this.setState({members: [...this.state.members,proj.data]}))
-    }}
-
-    await fetch('http://localhost:5000/api/consultancyagency/'+this.state.ID).then(res=>res.json())
-    .then(proj=>this.setState({CA:proj.data})).catch(this.setState({CA:null}))
-   
-      await fetch('http://localhost:5000/api/partners/'+this.state.ID).then(res=>res.json())
-    .then(proj=>this.setState({partner:proj.data}))
-  
-      await fetch('http://localhost:5000/api/admins/'+this.state.ID).then(res=>res.json())
-    .then(proj=>this.setState({admin:proj.data}))
-    
-    if(this.state.admin){
-      this.setState({type:'admin'})}    
-   if(this.state.partner){
-    this.setState({type:'partner'})}
-   if(this.state.CA){
-    this.setState({type:'CA'}) }
-    console.log(this.state.members)
+  async sendClicked() {
+    await axios
+      .post(
+        `http://localhost:5000/api/partners/${this.state.Owner._id}/rating/${
+          this.state.items._id
+        }`
+      )
+      .then(res => {
+        console.log(res);
+        return res.data;
+      })
+      .then(json =>
+        this.setState({
+          showMessage: json.data
+        })
+      );
+        alert("Attendees are notified");
 
 
+  }
 
-    
+  async componentDidMount() {
+    await axios
+      .get(`http://localhost:5000/api/events/5c93e86e61fb9b030dc88b9e`)
+      .then(res => {
+        console.log(res);
+        return res.data;
+      })
+      .then(json =>
+        this.setState({
+          items: json.data
+        })
+      );
+    await axios
+      .get(`http://localhost:5000/api/partners/${this.state.items.requestorId}`)
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+      .then(json =>
+        this.setState({
+          Owner: json.data,
+          isLoaded: true
+        })
+      );
+  }
+  returnClicked  = ()=>{
+    let path = `/myProfile/${this.state.Owner._id}`;
+    this.props.history.push({
+      pathname : path
+    });
+  }
+  render() {
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          <Card className={"text-center"} style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Requested By: {this.state.Owner.name}</Card.Title>
+              <Card.Text>Type: {this.state.items.eventType}</Card.Text>
+              <Card.Text>Description: {this.state.items.description}</Card.Text>
+              <Card.Text>Speaker: {this.state.items.speaker}</Card.Text>
+              <Card.Text>Date: {this.state.items.eventDate}</Card.Text>
+              <Button onClick={this.sendClicked.bind(this)} variant="primary">
+                Send FeedBack
+              </Button>
+              <Button onClick={this.returnClicked.bind(this)} variant="danger">
+                return to My Profile
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Card className={"text-center"} style={{ width: "32rem" }}>
+            <Card.Body>
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            </Card.Body>
+          </Card>
+        </div>
+      );
     }
-
-  render(){
-    const { classes } = this.props;
-    var name =null
-    if(this.state.admin){
-      name=this.state.admin.firstName}    
-   if(this.state.partner){
-    name=this.state.partner.firstName}
-   if(this.state.CA){
-    name=this.state.CA.name }
-    const event=this.state.event;
-
-  return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title}   gutterBottom>
-         Event Info
-        </Typography>
-        <Typography className={classes.pos}  >
-        Requested By : {name}
-        <br></br>
-        Description : {(event.description)?event.description:"None"}
-        <br></br>
-        <Box component="span" display="block">block</Box>
-        <div >
-        Event Location : {(event.eventLocation)?event.eventLocation:"None"}
-        <br></br>
-        Registeration Price : {(event.registPrice)?event.registPrice:"None"}
-        < br></br>
-        Remaining Places : {(event.remainingPlace)?event.remainingPlace:"None"}
-        <br></br>
-        Event Topics : {(event.topics)?event.topics.toString():"None"}
-        < br></br>
-        Speaker: {(event.speaker)?event.speaker:"None"}
-        <br></br>
-        Feedback : {(event.feedback)?event.feedback.toString():"None"}
-        <br></br>
-        Registeration Start Date : {(event.regist_start_date)?(new Date(event.regist_start_date)).getDate()+"/"+((new Date(event.regist_start_date)).getMonth()+1)+"/"+(new Date(event.regist_start_date)).getFullYear():"None"}
-        <br></br>
-        Registeration End Date: {(event.regist_expiry_date)?(new Date(event.regist_expiry_date)).getDate()+"/"+((new Date(event.regist_expiry_date)).getMonth()+1)+"/"+(new Date(event.regist_start_date)).getFullYear():"None"}
-        <br></br>
-        Event Date : {(event.eventDate)?(new Date(event.eventDate)).getDate()+"/"+((new Date(event.eventDate)).getMonth()+1)+"/"+(new Date(event.regist_start_date)).getFullYear():"None"}
-        <br></br>
-        Form Link: {(event.formLink)?event.formLink:"None"}
-        <br></br>
-        <br></br>
-        bookedMembers :
-        <GridList cellHeight={50} className={classes.gridList} cols={2}>
-        {this.state.members.map(mem => (
-          <GridListTile key={mem._id} className={classes.griditem}>
-            <Button text="10px"> {mem.fname}  {mem.mname} </Button>
-          </GridListTile>
-          ))}
-         </GridList>
-         </div>
-         
-        </Typography>
-        
-      </CardContent>
-      <CardActions>
-        <Button className={classes.root} size="small">Show More</Button>
-      </CardActions>
-    </Card>
-  );}
+  }
 }
-
-SimpleCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SimpleCard);
+export default (feedBackSending);
