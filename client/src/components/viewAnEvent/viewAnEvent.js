46592aa2c9b92f2c249b52e71ee256185e023e2a
omarkhaled998 <omarkhaled98@hotmail.com>
2019-04-12 14:51:10 +0200

    Update viewAnEvent.js

--- client/src/components/viewAnEvent/viewAnEvent.js
+++ client/src/components/viewAnEvent/viewAnEvent.js
@@ -4,29 +4,52 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Route  from "react-router-dom/Route";
 import {Card} from "react-bootstrap";
 
-class viewAnEvent extends React.Component {
+class viewAnEvent extends Component {
     constructor(props){
         super(props);
         this.state = {
         eventId : this.props.id ,
-        items : null , 
-        isLoaded : false 
+        items : [] , 
+        isLoaded : false,
+        RequestorId :[],
+        CA :[],
+        Admin:[],
+        partner: [],
+        mems:[],
+        members:[]
+         
     };
 
     }
     
 
-    componentDidMount() {
-        axios
-          .get(`http://localhost:5000/api/events/${this.state.eventId}`)
-          .then(res => {
-            return res.data;
-          })
-          .then(a => this.setState({ items: a.data , isLoaded : true}));
+    async componentDidMount() {
+       
+        await fetch('http://localhost:5000/api/events/'+this.state.eventId).then(res=>res.json())
+          .then(proj=>this.setState({items: proj.data ,isLoaded : true,mems:proj.data.bookedMembers}))
+          
+    // await fetch('http://localhost:5000/api/partners/'+this.state.RequestorId).then(res=>res.json())
+    //    .then(proj=>this.setState({partner:proj.data}))
+
+    //    if(this.state.partner.length===0){
+    //     await  fetch('http://localhost:5000/api/consultancyagency/'+this.state.RequestorId).then(res=>res.json())
+    //    .then(proj=>this.setState({CA:proj.data}))}
+         
+    //   if(this.state.CA.length===0&&this.state.partner.length===0){
+       
+    //      await fetch('http://localhost:5000/api/admins/'+this.state.RequestorId).then(res=>res.json())
+    //    .then(proj=>this.setState({Admin:proj.data}))
+    //   }  
+    for(var i=0;i<this.state.mems.length;i++){
+       await  fetch('http://localhost:5000/api/members/'+this.state.mems[i]).then(res=>res.json())
+      .then(proj=>this.setState({members: [...this.state.members,proj.data]}))
+      }
       }
 
       render(){
           const event = this.state.items ;
+          const member =this.state.members;
+        //  const name =(this.state.CA.length===0)?((this.state.Admin.length===0)?(this.state.partner.firstName):this.state.Admin.firstName):this.state.CA.name
           if(this.state.isLoaded)
          return (
              <div className = "App">
@@ -35,7 +58,7 @@ class viewAnEvent extends React.Component {
                      <Card.Body>
                          <Card.Title>{event.eventType}</Card.Title>
                          <Card.Text>
-                             Requested By : {(event.requestedBy)?event.requestedBy:"None"}
+                             {/* Requested By : {(event.requestorId)?event.requestorId:"None"} */}
                              <br></br>
                              Event Location : {(event.eventLocation)?event.eventLocation:"None"}
                              <br></br>
@@ -58,6 +81,9 @@ class viewAnEvent extends React.Component {
                              Event Date : {(event.eventDate)?(new Date(event.eventDate)).getDate()+"/"+((new Date(event.eventDate)).getMonth()+1)+"/"+(new Date(event.regist_start_date)).getFullYear():"None"}
                              <br></br>
                              Form Link: {(event.formLink)?event.formLink:"None"}
+                             <br></br>
+                             <br></br>
+                             bookedMembers : <ul>{this.state.members.map(s=><li key={s._id}>{s}</li>)}</ul>)}
 
                          </Card.Text>
                      </Card.Body>
@@ -67,7 +93,7 @@ class viewAnEvent extends React.Component {
          )
          else 
          return (
-             <label>Loading.. H</label>
+             <label>Loading...</label>
          ) 
 
       }
