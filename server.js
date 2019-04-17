52a8270e7f57233e6b4b79e9480bbd1e03767134
aashraaf <46944141+aashraaf@users.noreply.github.com>
2019-04-17 15:35:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
   if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'))
   
-    app.get('*',(req,res)=>{
+    app.use('*',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
   }
