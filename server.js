8d47228912a44b6af2bd3ad0d9fecbdc62c85a96
nadaelarabyy <nadaelarabyy@yahoo.com>
2019-04-18 19:02:31 +0200

    trying to make it work woth the deplo

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(function(req, res, next) {
     next();
   });
   if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname,'client/build')))
   
-    app.use('*',(req,res)=>{
+    app.get('*',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
   }
