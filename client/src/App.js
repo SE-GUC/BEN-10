17b532a7fa4d6a66f87c6f37b32b293e73f1bc0d
nihalmrashed <nehalmrashed@aol.com>
2019-04-22 20:05:52 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -236,12 +236,12 @@ class App extends Component {
     } else {
       return (
         <>
-      {/*    <a href='http://localhost:5000/profile#/'>click the link and choose type</a>
+         <a href='http://localhost:5000/profile#/'>click the link and choose type</a>
           <RedirectButton onClick={this.asAdmin} as={"Login as Admin"} />
           <RedirectButton onClick={this.asPartner} as={"Login as Partner"} />
           <RedirectButton onClick={this.asMember} as={"Login as Member"} />
-      <RedirectButton onClick={this.asCA} as={"Login as CA"} /> */} 
-      <SignUp></SignUp> 
+      <RedirectButton onClick={this.asCA} as={"Login as CA"} /> 
+      {/* <SignUp></SignUp>  */}
       
       
 
