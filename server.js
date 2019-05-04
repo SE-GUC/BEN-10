28511fe4645c2ce0531f4bd34ca2bbb9cccbb8ad
aashraaf <abdelrahman.ashraf@student.guc.edu.eg>
2019-05-04 13:57:28 +0200

    hi

--- server.js
+++ server.js
@@ -37,14 +37,6 @@ if (process.env.NODE_ENV === "production") {
   app.use("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-  if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'client/build')))
-  
-    app.get('*',(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-  }
-  // DB Config
 }
 // DB Config
 const db = require("./config/keys").mongoURI;
